a11b7e94e0fc593178670b24cc5c0901f83a178b
Steve Hipwell <steve.hipwell@gmail.com>
2026-09-16 10:37:00 +0100

    fixup! chore: Update go-github to 91.0.0

--- github/resource_github_organization_repository_role.go
+++ github/resource_github_organization_repository_role.go
@@ -12,6 +12,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-log/tflog"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+
 	"github.com/integrations/terraform-provider-github/v6/internal/tfpluginv2util"
 )
 
